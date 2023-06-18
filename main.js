4c06fe81aa20084024adbc2536202be20f229cfb
Shayan Owji <shayanalfa5@gmail.com>
2023-06-18 11:39:06 +0330

    new push

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
     //    analog clock
 
-const secondsHand = document.getElementById("seconds-hand")
-const minutesHand = document.getElementById("minutes-hand")
-const hoursHand = document.getElementById("hours-hand")
+const secondsHand = document.getElementById("seconds-hand");
+const minutesHand = document.getElementById("minutes-hand");
+const hoursHand = document.getElementById("hours-hand");
 
 function getTime() {
     const date = new Date();
@@ -12,14 +12,14 @@ function getTime() {
     const timeInterval = 6 ;
 
 
-    secondsHand.style.transform = 'rotate(' + (seconds * timeInterval) + 'deg )'
-    minutesHand.style.transform = 'rotate(' + (minutes * timeInterval + seconds / 10) + 'deg )'
-    hoursHand.style.transform = 'rotate(' + (hours * 30 + minutes / 2 ) + 'deg )'
+    secondsHand.style.transform = 'rotate(' + (seconds * timeInterval) + 'deg )';
+    minutesHand.style.transform = 'rotate(' + (minutes * timeInterval + seconds / 10) + 'deg )';
+    hoursHand.style.transform = 'rotate(' + (hours * 30 + minutes / 2 ) + 'deg )';
     
     
 }
 
-setInterval(getTime , 1000)
+setInterval(getTime , 1000);
        
        
        
@@ -27,29 +27,29 @@ setInterval(getTime , 1000)
        
     // digital clock
 
-// function showTime() {
-//     let date = new Date();
-//     let hours = date.getHours();
-//     let minutes = date.getMinutes();
-//     let seconds = date.getSeconds();
-//     let amPm = "AM"
+function showTime() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let amPm = "AM"
 
-//     if(hours === 0) {
-//         hours = 12;
-//     }
-//     if(hours > 12) {
-//         hours -= 12;
-//         amPm = "PM"
-//     }
+    if(hours === 0) {
+        hours = 12;
+    }
+    if(hours > 12) {
+        hours -= 12;
+        amPm = "PM"
+    }
 
-//     hours = (hours < 10 ) ? `0${hours}` : hours;
-//     minutes = (minutes < 10 ) ? `0${minutes}` : minutes;
-//     second = (seconds < 10 ) ? `0${seconds}` : seconds;
+    hours = (hours < 10 ) ? `0${hours}` : hours;
+    minutes = (minutes < 10 ) ? `0${minutes}` : minutes;
+    second = (seconds < 10 ) ? `0${seconds}` : seconds;
 
-//     let time = `${hours}:${minutes}:${seconds} ${amPm}`
-//     document.querySelector(".digital-clock").innerText = time;
-// }
+    let time = `${hours}:${minutes}:${seconds} ${amPm}`
+    document.querySelector(".digital-clock").innerText = time;
+}
 
-// setInterval(showTime , 1000);
+setInterval(showTime , 1000);
 
         // digital clock end
