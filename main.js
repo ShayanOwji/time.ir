14fd089edb56fb96c8d81c4d924f3f99c91d2959
Shayan Owji <shayanalfa5@gmail.com>
2023-07-03 20:22:46 +0330

    calendar modal section added

--- main.js
+++ main.js
@@ -54,3 +54,20 @@ setInterval(showTime , 1000);
 
         // digital clock end
 
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("modalBtn-right");
+const closeBtn = document.querySelector(".close-btn");
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
